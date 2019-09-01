2b05ab267cd095ccb009608b3ee1712b11df3655
v-bc-shashi <shashi.prashad@gmail.com>
2019-09-01 15:49:49 +0530

    Commiting the array sumInArray file

---
+++ com/spp/array/SumInArray.java
@@ -0,0 +1,35 @@
+package com.spp.array;
+
+// Given an Array and key, find that any sum of two element is equal to key . array is sorted
+public class SumInArray {
+
+    public static boolean findSum(int arr[], int key) {
+
+        if (arr.length == 0)
+            return false;
+        for (int i = 0, j = arr.length - 1; i < j;) {
+
+            if (arr[i] + arr[j] == key) {
+                System.out.println("the Index for elements are- [" + i + " , " + j +" ]");
+                return true;
+            } else if (arr[i] + arr[j] < key) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+
+        return false;
+    }
+
+    public static void main(String[] args) {
+
+        int arr[]= {3,4,5,6,7,8,9,10,11,20,30,32,34,40};
+    //    System.out.println(findSum(arr, 43)); // true
+    //    System.out.println(findSum(arr, 41));// false;
+        System.out.println(findSum(arr, 74));
+        System.out.println(findSum(arr, 21));
+        System.out.println(findSum(arr, 11));
+
+    }
+}
